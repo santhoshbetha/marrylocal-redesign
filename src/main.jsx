0249b8cfa76421e60c788bcdce882a4a70ace8b7
Santhosh Betha <santhosh.betha@gmail.com>
2026-01-14 15:27:54 -0600

    service worker

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import authReducer from './store/reducers/authReducer';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import registerServiceWorker from './serviceWorkerRegistration';
 import './index.css';
 import App from './App.jsx';
 
@@ -51,3 +52,5 @@ createRoot(document.getElementById('root')).render(
     </AuthProvider>
   </StrictMode>,
 );
+
+registerServiceWorker();
