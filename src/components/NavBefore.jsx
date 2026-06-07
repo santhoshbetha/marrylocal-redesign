14088feb8e9fea431f82efc737d865ed954344ab
Santhosh Betha <santhosh.betha@gmail.com>
2026-06-07 09:01:02 -0500

    title font and size change

--- src/components/NavBefore.jsx
+++ src/components/NavBefore.jsx
@@ -21,7 +21,7 @@ export function NavBefore({ openLogin, setOpenLogin }) {
       <div className="container mx-auto px-4 h-16 flex items-center justify-between">
         <Link to="/" className="flex items-center space-x-2 group">
           <img src="/logogold.png" alt="Logo" className="h-8 w-8" />
-          <span className="font-custom text-yellow-300">MarryLocal</span>
+          <span className="font-custom">MarryLocal</span>
         </Link>
 
         {/* Desktop Navigation */}
